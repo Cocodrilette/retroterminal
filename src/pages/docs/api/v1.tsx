f9464404161c23068d80120f392a8930a7559acd
cocodrilette <cocodrilette@gmail.com>
2026-02-27 21:16:58 -0500

    Updates

--- src/pages/docs/api/v1.tsx
+++
@@ -1,8 +0,0 @@
-export default function ApiV1() {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold mb-4">API_V1.SYS</h1>
-      <p>Endpoints for the RetroTerminal API.</p>
-    </div>
-  );
-}
